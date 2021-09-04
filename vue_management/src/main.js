62e391c9b3d43cba7a89db2e87e217be8cafc212
WhiteCoffee9834 <cst1374481664@gmail.com>
2021-09-04 16:46:46 +0900

    style

--- vue_management/src/main.js
+++ vue_management/src/main.js
@@ -5,7 +5,7 @@ import store from './store'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
-import axios from "axios"
+import axios from "./interceptor/interceptor.js"
 Vue.prototype.$axios = axios
 
 Vue.use(ElementUI);
