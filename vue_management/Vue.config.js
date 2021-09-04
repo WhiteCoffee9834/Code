f9b7d505f82bcff8682320f18e94d932789be6d2
WhiteCoffee9834 <cst1374481664@gmail.com>
2021-09-04 16:48:03 +0900

    feat: 可以获取到图片了

--- vue_management/Vue.config.js
+++ vue_management/Vue.config.js
@@ -4,6 +4,10 @@ module.exports = {
             "/api":{
                 target:"http://localhost:3000",
                 changeOrigin:true
+            },
+            "/uploads":{
+                target:"http://localhost:3000",
+                changeOrigin:true
             }
         }
     }
