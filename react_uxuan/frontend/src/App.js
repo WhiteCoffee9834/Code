af952c5a9b980079ffaf161715289c30f54624ef
WhiteCoffee9834 <cst1374481664@gmail.com>
2021-09-17 11:43:11 +0900

    feat: 添加底部导航显示切换功能 更改为类组件

--- react_uxuan/frontend/src/App.js
+++ react_uxuan/frontend/src/App.js
@@ -6,27 +6,50 @@ import Category from "./components/Category"
 import ShopCart from "./components/ShopCart"
 import Person from "./components/Person"
 import NotFound from "./components/NotFound"
+import Detail from './components/Detail';
 import Interceptor from "./components/Interceptor"
-function App() {
-    return (
-        <div className = "App" >
+import React, { Component } from 'react'
+class App extends Component {
+    state={
+        flag:true
+    }
+    componentDidMount(){
+        this.checkVisible(this.props.location.pathname)
+        this.props.history.listen(location=>{
+            this.checkVisible(location.pathname)
+        })
+    }
+    // 底部导航是否显示?
+    checkVisible(name){
+        let flag = true
+        name.includes("detail") ? flag = false : flag = true
+        this.setState({
+            flag
+        })
+    }
+
+    render() {
+        return (
+            <div className = "App" >
             <Switch>
                 <Route path="/login" component={Login}></Route>
                 <Route path="/homepage" component={Homepage}></Route>
                 <Route path="/category" component={Category}></Route>
+                <Route path="/detail/:id" component={Detail}></Route>
                 <Interceptor path="/shopcart" component={ShopCart}></Interceptor>
                 <Route path="/person" component={Person}></Route>
                 <Redirect from="/" to="/homepage" exact></Redirect>
                 <Route component={NotFound}></Route>
             </Switch>
-            <footer>
+            <footer style={{display: this.state.flag ? "flex" : "none"}}>
                 <NavLink to="/homepage" className="menu"><i></i></NavLink>
                 <NavLink to="/category" className="menu"><i></i></NavLink>
                 <NavLink to="/shopcart" className="menu"><i></i></NavLink>
                 <NavLink to="/person" className="menu"><i></i></NavLink>
             </footer>
         </div>
-    );
+        )
+    }
 }
 
 export default withRouter(App);
